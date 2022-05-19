170200261b55e049b6813b254a0449dbf7cd3fcb
iqbalrief <iqbal98arief@gmail.com>
2022-05-19 15:07:36 +0700

    crud bank

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const adminController = require('../controller/adminController')
+const { upload } = require('../middlewares/multer')
+
+router.get('/dashboard', adminController.viewDashboard);
+// ednpoint category
+router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+// endpoint Bank
+router.get('/bank', adminController.viewBank);
+router.post('/bank', upload, adminController.addBank);
+router.put('/bank', upload, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
+router.get('/item', adminController.viewItem);
+router.get('/booking', adminController.viewBooking);
+
+module.exports = router;  
