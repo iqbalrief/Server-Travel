22819ef717195aec4b3c2460f15bc65426e1c215
iqbalrief <iqbal98arief@gmail.com>
2022-05-31 15:34:08 +0700

    crud Feature

--- controller/adminController.js
+++ controller/adminController.js
@@ -2,6 +2,7 @@ const Category = require('../models/Category');
 const Bank = require('../models/Bank');
 const Item = require('../models/Item');
 const Image = require('../models/image');
+const Feature = require('../models/Feature');
 const fs = require('fs-extra');
 const path = require('path')
 
@@ -157,7 +158,7 @@ module.exports = {
         req.flash('alertMessage', `${error.message}`)
         req.flash('alertStatus', 'danger')
         res.redirect('/admin/bank')
-    }
+        }
     },
 
     viewItem: async (req, res) => {
@@ -332,6 +333,111 @@ module.exports = {
           res.redirect('/admin/item');
         }
     }, 
+
+    viewDetailItem: async (req, res) => {
+        try {
+            const { itemId } = req.params;
+            const alertMessage = req.flash('alertMessage');
+            const alertStatus = req.flash('alertStatus');
+            const feature = await Feature.find({ itemId: itemId})
+            
+            const alert = { message: alertMessage, status: alertStatus };
+            res.render('admin/item/detail_item/view_detail_item', {
+                title: 'Staycation | Detail Item',
+                alert,
+                itemId,
+                feature
+            }) 
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect(`/admin/item/show-detail-item/${itemId}`);
+        }
+    },
+    
+    addFeature: async (req, res) => {
+        const { name, qty, itemId } = req.body;
+        try {
+            if(!req.file) {
+                req.flash('alertMessage', 'Image not found');
+                req.flash('alertStatus', 'danger');
+                res.redirect(`/admin/item/show-detail-item/${itemId}`);
+            } 
+            const feature = await Feature.create({
+                name,
+                qty,
+                itemId,
+                imageUrl: `images/${req.file.filename}`
+            });
+
+            const item = await Item.findOne({ _id: itemId})
+            item.featureId.push({ _id: feature._id });
+            await item.save();
+            req.flash('alertMessage', 'Success Add Feature')
+            req.flash('alertStatus', 'success')
+            res.redirect(`/admin/item/show-detail-item/${itemId}`);
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect(`/admin/item/show-detail-item/${itemId}`);
+        }
+    },
+
+    editFeature: async (req, res) => {
+
+        try {
+
+            const { id, name, qty, itemId } = req.body;
+           
+            const feature = await Feature.findOne({ _id: id });
+            if (req.file == undefined) {
+                feature.name = name;
+                feature.qty = qty;
+                await feature.save();
+                req.flash('alertMessage', 'Success Update Feature')
+                req.flash('alertStatus', 'success')
+                res.redirect(`/admin/item/show-detail-item/${itemId}`);
+            } else {
+                await fs.unlink(path.join(`public/${feature.imageUrl}`));
+                feature.name = name;
+                feature.qty = qty;
+                feature.imageUrl = `images/${req.file.filename}`
+                await feature.save();
+                req.flash('alertMessage', 'Success Update Feature')
+                req.flash('alertStatus', 'success')
+                res.redirect(`/admin/item/show-detail-item/${itemId}`);
+            }
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect(`/admin/item/show-detail-item/${itemId}`);
+        }
+    },
+
+    deleteFeature: async (req, res) => {
+        const { id, itemId } = req.params;
+        try {
+            const feature = await Feature.findOne({ _id: id});
+
+            const item = await Item.findOne({ _id: itemId}).populate('featureId')
+            for(let i = 0; i < item.featureId.length; i++) {
+                if(item.featureId[i]._id.toString() === feature._id.toString()) {
+                    item.featureId.pull({ _id: feature._id });
+                    await item.save();
+                }
+            }
+            await fs.unlink(path.join(`public/${feature.imageUrl}`));
+            await feature.remove();
+            req.flash('alertMessage', 'Success Delete Feature')
+            req.flash('alertStatus', 'success')
+            res.redirect(`/admin/item/show-detail-item/${itemId}`);
+        } catch (error) {
+            console.log(error)
+            req.flash('alertMessage', `${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect(`/admin/item/show-detail-item/${itemId}`);
+            }
+        },
     
     viewBooking: (req, res) => {
         res.render('admin/booking/view_booking', {
--- routes/admin.js
+++ routes/admin.js
@@ -3,16 +3,19 @@ const adminController = require('../controller/adminController')
 const { uploadSingle, uploadMultiple } = require('../middlewares/multer')
 
 router.get('/dashboard', adminController.viewDashboard);
+
 // ednpoint category
 router.get('/category', adminController.viewCategory);
 router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editCategory);
 router.delete('/category/:id', adminController.deleteCategory);
+
 // endpoint Bank
 router.get('/bank', adminController.viewBank);
 router.post('/bank', uploadSingle, adminController.addBank);
 router.put('/bank', uploadSingle, adminController.editBank);
 router.delete('/bank/:id', adminController.deleteBank);
+
 // endpoint item
 router.get('/item', adminController.viewItem);
 router.post('/item', uploadMultiple, adminController.addItem);
@@ -20,6 +23,12 @@ router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
 router.delete('/item/:id/delete', adminController.deleteItem);
+
+// endpoint detail Item
+router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
+router.post('/item/add/feature', uploadSingle, adminController.addFeature);
+router.put('/item/update/feature', uploadSingle, adminController.editFeature);
+router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
 
 router.get('/booking', adminController.viewBooking);
 
